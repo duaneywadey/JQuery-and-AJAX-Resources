2cf4e54ad7f3e9af002b386189132f50aa020ffc
duaneywadey <idrdequito.cvt@eac.edu.ph>
2024-06-01 16:54:16 +0800

    new changes to the ajax codes

---
+++ AJAX/livesearchbootstrap/backend-search.php
@@ -0,0 +1,40 @@
+<?php  
+$host = 'localhost';
+$dbname = 'test';
+$username = 'root';
+$password = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
+
+$pdo = new PDO($dsn, $username, $password);
+if (isset($_REQUEST['searchInput'])) {
+    $searchInput = $_REQUEST['searchInput'];
+    $users = selectAllUsers($pdo, $searchInput);
+
+    if (!empty($users)) {
+        foreach ($users as $row) {
+            echo "
+                <div class='col-md-4 mt-4'>
+                    <div class='card'>
+                        <div class='card-body'>
+                            <h1>{$row['username']}</h1>
+                        </div>
+                    </div>
+                </div>
+            ";
+        }
+    } else {
+        echo "No data available";
+    }
+}
+
+function selectAllUsers($pdo, $searchInput) {
+    $sql = "SELECT * FROM users WHERE username LIKE ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(["%$searchInput%"]);
+    return $stmt->fetchAll();
+}
+
+
+?>
