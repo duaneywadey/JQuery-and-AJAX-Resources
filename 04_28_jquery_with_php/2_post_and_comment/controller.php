db9c18d238999ea7af7e085b9bfb8f9a430df5f0
duaneywadey <idrdequito.cvt@eac.edu.ph>
2025-04-27 13:39:28 +0800

    add simplified implementation

--- 04_28_jquery_with_php/2_post_and_comment/controller.php
+++ 04_28_jquery_with_php/2_post_and_comment/controller.php
@@ -11,21 +11,6 @@ if (isset($_POST['createNewComment'])) {
 	$commentDescInputField = $_POST['commentDescInputField'];
 	$postID = $_POST['postID'];
 	insertAComment($pdo, $postID, $commentDescInputField);
-	$getAllCommentsByPostID = getAllCommentsByPostID($pdo, $postID);
-	foreach ($getAllCommentsByPostID as $row) {
-		echo '<div class="commentContainer" style="margin-top: 10px;">
-						<p class="commentDescription">' . $row['comment_desc'] . '</p>
-						<form class="editCommentForm d-none">
-							<input type="hidden" class="commentIDEditField" 
-							value="' . $row['mock_comment_id'] . '"
-							>
-							<input type="text" class="commentDescEditField"
-							value="' . $row["comment_desc"] . '"
-							>
-							<input type="submit" value="Submit">
-						</form>
-					</div>';
-	}
 }
 
 if (isset($_POST['editPost'])) {
@@ -39,22 +24,6 @@ if (isset($_POST['editComment'])) {
 	$commentIDEditField = $_POST['commentIDEditField'];
 	$postIDForEdit = $_POST['postIDForEdit'];
 	updateAComment($pdo, $commentDescEditField, $commentIDEditField);
-	$getAllCommentsByPostID = getAllCommentsByPostID($pdo, $postIDForEdit);
-	foreach ($getAllCommentsByPostID as $row) {
-		echo '<div class="commentContainer" style="margin-top: 10px;">
-						<p class="commentDescription">' . $row['comment_desc'] . '</p>
-						<form class="editCommentForm d-none">
-							<input type="hidden" class="commentIDEditField" 
-							value="' . $row['mock_comment_id'] . '"
-							>
-							<input type="text" class="commentDescEditField"
-							value="' . $row["comment_desc"] . '"
-							>
-							<input type="submit" value="Submit">
-						</form>
-					</div>';
-	}
-
 }
 
 ?>
